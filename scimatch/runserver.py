b51526d339e9151e2389afa7e758e80c27201fe4
Demitri Muna <github@demitri.com>
2012-07-10 22:48:58 +0200

    More framework building.

--- scimatch/runserver.py
+++ scimatch/runserver.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from scimatch import app
-from scimatch.controllers import xxxx
+from scimatch.controllers import new_user
 
 @app.route('/')
 def home_page():
@@ -17,5 +17,5 @@ def home_page():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
 
